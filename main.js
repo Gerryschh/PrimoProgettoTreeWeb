76e2a5d77ed362b8cd4116f74c6555e9bceae4ac
Gerardo <gerardo.dambola261000@gmail.com>
2022-05-10 15:24:41 +0200

    Implemented First Person View

--- main.js
+++ main.js
@@ -5,516 +5,11 @@ import {GLTFLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/js
 import {OctahedronGeometry} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/src/geometries/OctahedronGeometry.js';
 import {MeshNormalMaterial} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/src/materials/MeshNormalMaterial.js';
 import {OBJLoader} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/loaders/OBJLoader.js';
+import {PointerLockControls} from 'https://cdn.jsdelivr.net/npm/three@0.118.1/examples/jsm/controls/PointerLockControls.js';
 
-class BasicCharacterControllerProxy {
-  constructor(animations) {
-    this._animations = animations;
-  }
-
-  get animations() {
-    return this._animations;
-  }
-};
-
-
-
-class BasicCharacterController {
-  constructor(params) {
-    this._Init(params);
-  }
-
-  _Init(params) {
-    this._params = params;
-    this._decceleration = new THREE.Vector3(-0.0005, -0.0001, -5.0);
-    this._acceleration = new THREE.Vector3(1, 0.25, 50.0);
-    this._velocity = new THREE.Vector3(0, 0, 0);
-    this._position = new THREE.Vector3();
-
-    this._animations = {};
-    this._input = new BasicCharacterControllerInput();
-    this._stateMachine = new CharacterFSM(
-        new BasicCharacterControllerProxy(this._animations));
-
-    this._LoadModels();
-  }
-
-  //It loads the character, its movements and its animation
-  _LoadModels() {
-    const loader = new FBXLoader();
-    loader.setPath('./resources/character/');
-    loader.load('ty.fbx', (fbx) => {
-      fbx.scale.setScalar(0.1);
-      fbx.traverse(c => {
-        c.castShadow = true;
-      });
-
-      this._target = fbx;
-      this._params.scene.add(this._target);
-
-      this._mixer = new THREE.AnimationMixer(this._target);
-
-      this._manager = new THREE.LoadingManager();
-      this._manager.onLoad = () => {
-        this._stateMachine.SetState('idle');
-      };
-
-      const _OnLoad = (animName, anim) => {
-        const clip = anim.animations[0];
-        const action = this._mixer.clipAction(clip);
-  
-        this._animations[animName] = {
-          clip: clip,
-          action: action,
-        };
-      };
-
-      const loader = new FBXLoader(this._manager);
-      loader.setPath('./resources/actions/');
-      loader.load('walk.fbx', (a) => { _OnLoad('walk', a); });
-      loader.load('run.fbx', (a) => { _OnLoad('run', a); });
-      loader.load('idle.fbx', (a) => { _OnLoad('idle', a); });
-      //loader.load('dance.fbx', (a) => { _OnLoad('dance', a); }); //Dance state removed for a bug
-    });
-  }
-
-  get Position() {
-    return this._position;
-  }
-
-  get Rotation() {
-    if (!this._target) {
-      return new THREE.Quaternion();
-    }
-    return this._target.quaternion;
-  }
-
-  Update(timeInSeconds) {
-    if (!this._stateMachine._currentState) {
-      return;
-    }
-
-    this._stateMachine.Update(timeInSeconds, this._input);
-
-    const velocity = this._velocity;
-    const frameDecceleration = new THREE.Vector3(
-        velocity.x * this._decceleration.x,
-        velocity.y * this._decceleration.y,
-        velocity.z * this._decceleration.z
-    );
-    frameDecceleration.multiplyScalar(timeInSeconds);
-    frameDecceleration.z = Math.sign(frameDecceleration.z) * Math.min(
-        Math.abs(frameDecceleration.z), Math.abs(velocity.z));
-
-    velocity.add(frameDecceleration);
-
-    const controlObject = this._target;
-    const _Q = new THREE.Quaternion();
-    const _A = new THREE.Vector3();
-    const _R = controlObject.quaternion.clone();
-
-    const acc = this._acceleration.clone();
-    if (this._input._keys.shift) {
-      acc.multiplyScalar(5.0);
-    }
-
-    if (this._stateMachine._currentState.Name == 'dance') {
-      acc.multiplyScalar(0.0);
-    }
-
-    if (this._input._keys.forward) {
-      velocity.z += acc.z * timeInSeconds;
-    }
-    if (this._input._keys.backward) {
-      velocity.z -= acc.z * timeInSeconds;
-    }
-    if (this._input._keys.left) {
-      _A.set(0, 1, 0);
-      _Q.setFromAxisAngle(_A, 4.0 * Math.PI * timeInSeconds * this._acceleration.y);
-      _R.multiply(_Q);
-    }
-    if (this._input._keys.right) {
-      _A.set(0, 1, 0);
-      _Q.setFromAxisAngle(_A, 4.0 * -Math.PI * timeInSeconds * this._acceleration.y);
-      _R.multiply(_Q);
-    }
-
-    controlObject.quaternion.copy(_R);
-
-    const oldPosition = new THREE.Vector3();
-    oldPosition.copy(controlObject.position);
-
-    const forward = new THREE.Vector3(0, 0, 1);
-    forward.applyQuaternion(controlObject.quaternion);
-    forward.normalize();
-
-    const sideways = new THREE.Vector3(1, 0, 0);
-    sideways.applyQuaternion(controlObject.quaternion);
-    sideways.normalize();
-
-    sideways.multiplyScalar(velocity.x * timeInSeconds);
-    forward.multiplyScalar(velocity.z * timeInSeconds);
-
-    controlObject.position.add(forward);
-    controlObject.position.add(sideways);
-
-    this._position.copy(controlObject.position);
-
-    if (this._mixer) {
-      this._mixer.update(timeInSeconds);
-    }
-  }
-};
-
-//
-class BasicCharacterControllerInput {
-  constructor() {
-    this._Init();    
-  }
-
-  _Init() {
-    this._keys = {
-      forward: false,
-      backward: false,
-      left: false,
-      right: false,
-      space: false,
-      shift: false,
-    };
-    document.addEventListener('keydown', (e) => this._onKeyDown(e), false);
-    document.addEventListener('keyup', (e) => this._onKeyUp(e), false);
-  }
-
-  _onKeyDown(event) {
-    switch (event.keyCode) {
-      case 87: // w
-        this._keys.forward = true;
-        break;
-      case 65: // a
-        this._keys.left = true;
-        break;
-      case 83: // s
-        this._keys.backward = true;
-        break;
-      case 68: // d
-        this._keys.right = true;
-        break;
-      case 32: // SPACE
-        this._keys.space = true;
-        break;
-      case 16: // SHIFT
-        this._keys.shift = true;
-        break;
-    }
-  }
-
-  _onKeyUp(event) {
-    switch(event.keyCode) {
-      case 87: // w
-        this._keys.forward = false;
-        break;
-      case 65: // a
-        this._keys.left = false;
-        break;
-      case 83: // s
-        this._keys.backward = false;
-        break;
-      case 68: // d
-        this._keys.right = false;
-        break;
-      case 32: // SPACE
-        this._keys.space = false;
-        break;
-      case 16: // SHIFT
-        this._keys.shift = false;
-        break;
-    }
-  }
-};
-
-//Declaring stateMachine for character's states
-class FiniteStateMachine {
-  constructor() {
-    this._states = {};
-    this._currentState = null;
-  }
-
-  _AddState(name, type) {
-    this._states[name] = type;
-  }
-
-  SetState(name) {
-    const prevState = this._currentState;
-    
-    if (prevState) {
-      if (prevState.Name == name) {
-        return;
-      }
-      prevState.Exit();
-    }
-
-    const state = new this._states[name](this);
-
-    this._currentState = state;
-    state.Enter(prevState);
-  }
-
-  Update(timeElapsed, input) {
-    if (this._currentState) {
-      this._currentState.Update(timeElapsed, input);
-    }
-  }
-};
-
-
-class CharacterFSM extends FiniteStateMachine {
-  constructor(proxy) {
-    super();
-    this._proxy = proxy;
-    this._Init();
-  }
-
-  _Init() {
-    this._AddState('idle', IdleState);
-    this._AddState('walk', WalkState);
-    this._AddState('run', RunState);
-    //this._AddState('dance', DanceState); //Dance state removed for a bug
-  }
-};
-
-//Defining state class for Character's states
-class State {
-  constructor(parent) {
-    this._parent = parent;
-  }
-
-  Enter() {}
-  Exit() {}
-  Update() {}
-};
-
-//DanceState (When the character is dancing) //Dance state removed for a bug
-/*class DanceState extends State {
-  constructor(parent) {
-    super(parent);
-
-    this._FinishedCallback = () => {
-      this._Finished();
-    }
-  }
-
-  get Name() {
-    return 'dance';
-  }
-
-  Enter(prevState) {
-    const curAction = this._parent._proxy._animations['dance'].action;
-    const mixer = curAction.getMixer();
-    mixer.addEventListener('finished', this._FinishedCallback);
-
-    if (prevState) {
-      const prevAction = this._parent._proxy._animations[prevState.Name].action;
-
-      curAction.reset();  
-      curAction.setLoop(THREE.LoopOnce, 1);
-      curAction.clampWhenFinished = true;
-      curAction.crossFadeFrom(prevAction, 0.2, true);
-      curAction.play();
-    } else {
-      curAction.play();
-    }
-  }
-
-  _Finished() {
-    this._Cleanup();
-    this._parent.SetState('idle');
-  }
-
-  _Cleanup() {
-    const action = this._parent._proxy._animations['dance'].action;
-    
-    action.getMixer().removeEventListener('finished', this._CleanupCallback);
-  }
-
-  Exit() {
-    this._Cleanup();
-  }
-
-  Update(_) {
-  }
-};*/
-
-//WalkState(When the character is walking)
-class WalkState extends State {
-  constructor(parent) {
-    super(parent);
-  }
-
-  get Name() {
-    return 'walk';
-  }
-
-  Enter(prevState) {
-    const curAction = this._parent._proxy._animations['walk'].action;
-    if (prevState) {
-      const prevAction = this._parent._proxy._animations[prevState.Name].action;
-
-      curAction.enabled = true;
-
-      if (prevState.Name == 'run') {
-        const ratio = curAction.getClip().duration / prevAction.getClip().duration;
-        curAction.time = prevAction.time * ratio;
-      } else {
-        curAction.time = 0.0;
-        curAction.setEffectiveTimeScale(1.0);
-        curAction.setEffectiveWeight(1.0);
-      }
-
-      curAction.crossFadeFrom(prevAction, 0.5, true);
-      curAction.play();
-    } else {
-      curAction.play();
-    }
-  }
-
-  Exit() {
-  }
-
-  Update(timeElapsed, input) {
-    if (input._keys.forward || input._keys.backward) {
-      if (input._keys.shift) {
-        this._parent.SetState('run');
-      }
-      return;
-    }
-
-    this._parent.SetState('idle');
-  }
-};
-
-//RunState(WHen the character is running)
-class RunState extends State {
-  constructor(parent) {
-    super(parent);
-  }
-
-  get Name() {
-    return 'run';
-  }
-
-  Enter(prevState) {
-    const curAction = this._parent._proxy._animations['run'].action;
-    if (prevState) {
-      const prevAction = this._parent._proxy._animations[prevState.Name].action;
-
-      curAction.enabled = true;
-
-      if (prevState.Name == 'walk') {
-        const ratio = curAction.getClip().duration / prevAction.getClip().duration;
-        curAction.time = prevAction.time * ratio;
-      } else {
-        curAction.time = 0.0;
-        curAction.setEffectiveTimeScale(1.0);
-        curAction.setEffectiveWeight(1.0);
-      }
-
-      curAction.crossFadeFrom(prevAction, 0.5, true);
-      curAction.play();
-    } else {
-      curAction.play();
-    }
-  }
-
-  Exit() {
-  }
-
-  Update(timeElapsed, input) {
-    if (input._keys.forward || input._keys.backward) {
-      if (!input._keys.shift) {
-        this._parent.SetState('walk');
-      }
-      return;
-    }
-
-    this._parent.SetState('idle');
-  }
-};
-
-//IdleState(When the character is doing nothing)
-class IdleState extends State {
-  constructor(parent) {
-    super(parent);
-  }
-
-  get Name() {
-    return 'idle';
-  }
-
-  Enter(prevState) {
-    const idleAction = this._parent._proxy._animations['idle'].action;
-    if (prevState) {
-      const prevAction = this._parent._proxy._animations[prevState.Name].action;
-      idleAction.time = 0.0;
-      idleAction.enabled = true;
-      idleAction.setEffectiveTimeScale(1.0);
-      idleAction.setEffectiveWeight(1.0);
-      idleAction.crossFadeFrom(prevAction, 0.5, true);
-      idleAction.play();
-    } else {
-      idleAction.play();
-    }
-  }
-
-  Exit() {
-  }
-
-  Update(_, input) {
-    if (input._keys.forward || input._keys.backward) {
-      this._parent.SetState('walk');
-    } /*else if (input._keys.space) {
-      this._parent.SetState('dance');
-    }*/ //Dance state removed for a bug
-  }
-};
-
-//Defining ThirdPersonCamera for our character that follows what the player is looking at
-class ThirdPersonCamera {
-  constructor(params) {
-    this._params = params;
-    this._camera = params.camera;
-
-    this._currentPosition = new THREE.Vector3();
-    this._currentLookat = new THREE.Vector3();
-  }
-
-  _CalculateIdealOffset() {
-    const idealOffset = new THREE.Vector3(-15, 20, -30);
-    idealOffset.applyQuaternion(this._params.target.Rotation);
-    idealOffset.add(this._params.target.Position);
-    return idealOffset;
-  }
-
-  _CalculateIdealLookat() {
-    const idealLookat = new THREE.Vector3(0, 10, 50);
-    idealLookat.applyQuaternion(this._params.target.Rotation);
-    idealLookat.add(this._params.target.Position);
-    return idealLookat;
-  }
-
-  Update(timeElapsed) {
-    const idealOffset = this._CalculateIdealOffset();
-    const idealLookat = this._CalculateIdealLookat();
-  
-    const t = 1.0 - Math.pow(0.001, timeElapsed);
-
-    this._currentPosition.lerp(idealOffset, t);
-    this._currentLookat.lerp(idealLookat, t);
-    this._camera.position.copy(this._currentPosition);
-    this._camera.lookAt(this._currentLookat);
-  }
-}
 
 //Main class that instanciate all the other things
-class ThirdPersonCameraDemo {
+class TreeWebGame {
   constructor() {
     this._Initialize();
   }
@@ -536,13 +31,28 @@ class ThirdPersonCameraDemo {
     }, false);
 
     //Loading Camera
-    const fov = 60;
-    const aspect = 1920 / 1080;
-    const near = 1.0;
+    const fov = 65;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 1;
     const far = 10000.0;
     this._camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this._camera.position.set(50, 10, 25);
+    this._camera.position.set(50, 19, 25);
 
+    //Loading controls keys
+    this._fpsControls = new PointerLockControls( this._camera, this._threejs.domElement );
+    this._clock = new THREE.Clock();
+    document.body.addEventListener('click', ()=> {
+      this._fpsControls.lock();
+    })
+    this._keyboard = [];
+    document.addEventListener('keydown', (e) => {
+      this._keyboard[e.key] = true;
+    });
+    document.addEventListener('keyup', (e) => {
+      this._keyboard[e.key] = false;
+    });
+
+    //Loading scene
     this._scene = new THREE.Scene();
 
     //Loading OctahedronGeometry on NPC
@@ -604,9 +114,6 @@ class ThirdPersonCameraDemo {
     //Loading the plane
     this._LoadPlane();
 
-    //Loading Main Character
-    this._LoadAnimatedModel();
-
     //Loading IdleAnimatedModels
     this._LoadStaticModelEmptyZone();
     this._LoadStaticModelWaterZone();
@@ -627,6 +134,33 @@ class ThirdPersonCameraDemo {
 
     //RAF
     this._RAF();
+  }
+
+  //FirstPersonMovements Controls Function
+  _processKeyboard() {
+    let speed = 70;
+    let actualSpeed = speed * this._delta; 
+    if(this._keyboard['w']) {
+      this._fpsControls.moveForward(actualSpeed);
+    }
+    if (this._keyboard['s']) {
+      this._fpsControls.moveForward(-actualSpeed);
+    }
+    
+    //Up and down movements not used
+    /*if (this._keyboard['q']) {
+      this._fpsControls.getObject().position.y += actualSpeed;
+    }
+    if(this._keyboard['e']) {
+      this._fpsControls.getObject().position.y -= actualSpeed;
+    }*/
+
+    if(this._keyboard['a']) {
+      this._fpsControls.moveRight(-actualSpeed);
+    }
+    if (this._keyboard['d']) {
+      this._fpsControls.moveRight(actualSpeed);
+    }
   }
 
   //Function that loads the plane done with Blender
@@ -789,23 +323,6 @@ _LoadStaticModelCinemaZone() {
     });
 }
 
-
-
-//Function that loads the Main Character with its camera and controls
-  _LoadAnimatedModel() {
-    const params = {
-      camera: this._camera,
-      scene: this._scene,
-    }
-    this._controls = new BasicCharacterController(params);
-
-    this._thirdPersonCamera = new ThirdPersonCamera({
-      camera: this._camera,
-      target: this._controls,
-    });
-  }
-
-
   _OnWindowResize() {
     this._camera.aspect = window.innerWidth / window.innerHeight;
     this._camera.updateProjectionMatrix();
@@ -825,6 +342,9 @@ _LoadStaticModelCinemaZone() {
       this.NPCOctahedronMesh3.rotation.y +=0.017;
       this.NPCOctahedronMesh4.rotation.y +=0.017;
 
+      this._delta = this._clock.getDelta();
+      this._processKeyboard(this._delta);
+      
       this._threejs.render(this._scene, this._camera);
       this._Step(t - this._previousRAF);
       this._previousRAF = t;
@@ -836,10 +356,6 @@ _LoadStaticModelCinemaZone() {
     if (this._mixers) {
       this._mixers.map(m => m.update(timeElapsedS));
     }
-    if (this._controls) {
-      this._controls.Update(timeElapsedS);
-    }
-    this._thirdPersonCamera.Update(timeElapsedS);
   }
 }
 
@@ -847,5 +363,5 @@ _LoadStaticModelCinemaZone() {
 let _APP = null;
 
 window.addEventListener('DOMContentLoaded', () => {
-  _APP = new ThirdPersonCameraDemo();
+  _APP = new TreeWebGame();
 });
